504bba8658dc0ccef275926f456f202595020536
Fer <fer658798@gmail.com>
2024-01-02 21:04:54 -0600

    Version 1 restored

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Version 3");
-        Scanner scanner = new Scanner(System.in);
-        scanner.close();
+        System.out.println("Hello");
     }
 }

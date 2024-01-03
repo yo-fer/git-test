bd8af435631234da5787f6e11e91547427e542f4
Fer <fer658798@gmail.com>
2024-01-02 20:26:20 -0600

    Version 2 of all files

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello");
+        System.out.println("Version 2");
         Scanner scanner = new Scanner(System.in);
         scanner.close();
     }

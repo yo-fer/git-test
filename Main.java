8c16bed945b5cc27d4fab305dc375754ab74645d
Fer <fer658798@gmail.com>
2024-01-02 20:23:41 -0600

    Add Scanner object in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
     }
 }

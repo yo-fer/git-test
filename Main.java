36687df9b2b2b17ca59ef4d0394914b701ed12ca
Fer <fer658798@gmail.com>
2024-01-06 21:30:07 -0600

    Version 5

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Version 3");
+        System.out.println("Version 5");
         Scanner scanner = new Scanner(System.in);
         scanner.close();
     }
